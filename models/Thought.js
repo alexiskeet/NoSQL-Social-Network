a64f4a9cab90dca550b3b165f2f2befc777a1e73
Alexis <alexis_keeter@yahoo.com>
2023-05-17 22:47:06 -0700

    building schema for thought.js

--- models/Thought.js
+++ models/Thought.js
@@ -1,2 +1,33 @@
 const { Schema, model } = require('mongoose');
-const thoughtSchema = require('')
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            maxlength: 280,
+            minlength: 1,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            // use a getter method to format the timestamp on query
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: {
+            // array of nested documents created with the reactionSchema
+        }
+    },
+    {
+        toJSON: {
+          virtuals: true,
+        },
+      },
+);
+// create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query 
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
