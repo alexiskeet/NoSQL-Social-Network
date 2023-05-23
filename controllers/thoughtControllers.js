1b045aaf93b429df8ecb5dd2ad7947a159089728
Alexis <alexis_keeter@yahoo.com>
2023-05-22 17:08:20 -0700

    fixed bug with reactions post route

--- controllers/thoughtControllers.js
+++ controllers/thoughtControllers.js
@@ -72,7 +72,7 @@ module.exports = {
         try {
           const thought = await Thought.findOneAndUpdate(
             { _id: req.params.thoughtId },
-            { $addToSet: { reactions: req.body.reactionId } },
+            { $addToSet: { reactions: req.body } },
             { runValidators: true, new: true }
           );
     
